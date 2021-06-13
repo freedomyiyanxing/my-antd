8c79dc9da9610a0aef288847241a57f8fb8abe54
freedom.yi <851989962@qq.com>
2021-06-13 21:45:17 +0800

    feat: 放开eject, 配置eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,44 @@
 module.exports = {
-
-}
+  root: true,
+  extends: [
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 0,
+    'react/button-has-type': 0,
+    'react/prop-types': 0,
+    'no-console': 0,
+    'react/jsx-props-no-spreading': 0,
+    'prettier/prettier': [
+      2,
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      haha1122
-    </div>
-  );
-}
+const App = (): JSX.Element => {
+  return <div className="App">haha1122</div>;
+};
 
 export default App;
